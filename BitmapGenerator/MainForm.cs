190973edc0a3afac444bfaea1469a4b2234dddd1
TheGoodFella97 <galas.daniele@gmail.com>
2015-10-19 14:58:18 +0200

    Add keys shortcut form

--- BitmapGenerator/MainForm.cs
+++ BitmapGenerator/MainForm.cs
@@ -33,7 +33,7 @@ namespace BitmapGenerator
             rndbit.Width = pic.Width;
             rndbit.Height = pic.Height;
             pic.Image = rndbit.Generate();
-            this.Text = "Done - press ctrl+s to save - " + rndbit.Width + " * " + rndbit.Height;
+            this.Text = "Done - " + rndbit.Width + " * " + rndbit.Height;
         }
 
         private void MainForm_Resize(object sender, EventArgs e)
@@ -43,13 +43,30 @@ namespace BitmapGenerator
 
         private void MainForm_KeyDown(object sender, KeyEventArgs e)
         {
-            if (e.Control && e.KeyCode == Keys.S)
+            bool notPressed = true;
+            if (e.Control && e.KeyCode == Keys.S && notPressed)
             {
                 if (saveFileDialog.ShowDialog() == DialogResult.OK)
                 {
                     rndbit.SaveImage(saveFileDialog.FileName);
                     saveFileDialog.FileName = string.Empty;
                 }
+                notPressed = false;
+            }
+
+            if (e.KeyCode == Keys.Enter && notPressed)
+            {
+                SetPic();
+                notPressed = false;
+            }
+
+            if (e.KeyCode == Keys.F1)
+            {
+                MessageBox.Show("Keys shortcut"
+                    + Environment.NewLine
+                    + Environment.NewLine + "ctrl+s : save image"
+                    + Environment.NewLine + "ENTER : Refresh image"
+                    , "Keys shortcut", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
     }
